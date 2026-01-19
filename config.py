8c042ad38c78e0b6c9b13841fe2b65ee27e086b7
Ninad Joshi <ninad123@gmail.com>
2026-01-19 19:04:00 +0100

    Reset version to 0.001

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 # Version number - increment by 0.1 for each code change
-APP_VERSION = "1.5"
+APP_VERSION = "0.001"
 
 # Load environment variables (for local development)
 load_dotenv()
